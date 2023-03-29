370de3c279ce7ce8db3b8fbe7af8acbc047659c5
MrSk007 <106589344+MrSk007@users.noreply.github.com>
2023-03-29 11:46:17 +0530

    1 file modified

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -7,63 +7,31 @@ const MenuBar = ({ editor }) => {
 
   return (
     <>
-          <button
+      <button
         onClick={() => editor.chain().focus().setColor('#94FADB').run()}
-        className={editor.isActive('textStyle', { color: '#94FADB' }) ? 'is-active' : ''}
+        className={
+          editor.isActive('textStyle', { color: '#94FADB' }) ? 'is-active' : ''
+        }
       >
         teal
       </button>
       <button
         onClick={() => editor.chain().focus().setColor('#B9F18D').run()}
-        className={editor.isActive('textStyle', { color: '#B9F18D' }) ? 'is-active' : ''}
+        className={
+          editor.isActive('textStyle', { color: '#B9F18D' }) ? 'is-active' : ''
+        }
       >
         green
       </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? 'is-active' : ''}
-      >
-        bold
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? 'is-active' : ''}
-      >
-        italic
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
-      >
-        h2
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
-      >
-        h3
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
-      >
-        bullet list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
-      >
-        ordered list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? 'is-active' : ''}
-      >
-        code block
-      </button>
       <button onClick={() => editor.chain().focus().undo().run()}>undo</button>
       <button onClick={() => editor.chain().focus().redo().run()}>redo</button>
-      <button onClick={() => editor.commands.setContent(localStorage.getItem('editor'))}>save</button>
+      <button
+        onClick={() =>
+          editor.commands.setContent(localStorage.getItem('editor'))
+        }
+      >
+        save
+      </button>
     </>
   );
 };
